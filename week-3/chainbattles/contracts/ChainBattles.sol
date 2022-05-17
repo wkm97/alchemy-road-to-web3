a4fe38ff48ad751dbd304982e06f00404ac77358
karming.wan <karming1997@gmail.com>
2022-05-18 00:00:58 +0800

    week-3 added character attributes

--- week-3/chainbattles/contracts/ChainBattles.sol
+++ week-3/chainbattles/contracts/ChainBattles.sol
@@ -2,36 +2,84 @@
 pragma solidity ^0.8.0;
 
 // Demo Contract Address on Mumbai: 0x702F82Ad837793329ed27dA20836AaAa7D459A0f
+// Exercise Contract Address on Mumbai: 0xe012f7252C33B39f2aE86528366aa893023ecbe7
+
+import "hardhat/console.sol";
 
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/utils/Strings.sol";
 import "@openzeppelin/contracts/utils/Base64.sol";
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 
 contract ChainBattles is ERC721URIStorage {
     using Strings for uint256;
+    using SafeMath for uint256;
     using Counters for Counters.Counter;
+    uint256 randNonce = 0;
     Counters.Counter private _tokenIds;
 
-    mapping(uint256 => uint256) public tokenIdToLevels;
+    mapping(uint256 => Character) public tokenIdToCharacter;
+
+    struct Character {
+        string name;
+        string class;
+        uint256 level;
+        uint256 agility;
+        uint256 strength;
+        uint256 intelligence;
+    }
 
     constructor() ERC721("Chain Battles", "CBTLS") {}
+
+    function randMod(uint256 _modulus) internal returns (uint256) {
+        randNonce++;
+        return
+            uint256(
+                keccak256(
+                    abi.encodePacked(block.timestamp, msg.sender, randNonce)
+                )
+            ) % _modulus;
+    }
 
     function generateCharacter(uint256 tokenId)
         public
         view
         returns (string memory)
     {
+        string memory name;
+        string memory class;
+        uint256 level;
+        uint256 agility;
+        uint256 strength;
+        uint256 intelligence;
+        (name, class, level, agility, strength, intelligence) = getAttributes(tokenId);
         bytes memory svg = abi.encodePacked(
             '<svg xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMinYMin meet" viewBox="0 0 350 350">',
             "<style>.base { fill: white; font-family: serif; font-size: 14px; }</style>",
             '<rect width="100%" height="100%" fill="black" />',
+            '<text x="50%" y="20%" class="base" dominant-baseline="middle" text-anchor="middle">',
+            class,
+            "</text>",
+            '<text x="50%" y="30%" class="base" dominant-baseline="middle" text-anchor="middle">',
+            "Name: ",
+            name,
+            "</text>",
             '<text x="50%" y="40%" class="base" dominant-baseline="middle" text-anchor="middle">',
-            "Warrior",
+            "Level: ",
+            level.toString(),
             "</text>",
             '<text x="50%" y="50%" class="base" dominant-baseline="middle" text-anchor="middle">',
-            "Levels: ",
-            getLevels(tokenId),
+            "Agility: ",
+            agility.toString(),
+            "</text>",
+            '<text x="50%" y="60%" class="base" dominant-baseline="middle" text-anchor="middle">',
+            "Strength: ",
+            strength.toString(),
+            "</text>",
+            '<text x="50%" y="70%" class="base" dominant-baseline="middle" text-anchor="middle">',
+            "Intelligence: ",
+            intelligence.toString(),
             "</text>",
             "</svg>"
         );
@@ -44,9 +92,27 @@ contract ChainBattles is ERC721URIStorage {
             );
     }
 
-    function getLevels(uint256 tokenId) public view returns (string memory) {
-        uint256 levels = tokenIdToLevels[tokenId];
-        return levels.toString();
+    function getAttributes(uint256 tokenId)
+        public
+        view
+        returns (
+            string memory name,
+            string memory class,
+            uint256 level,
+            uint256 agility,
+            uint256 strength,
+            uint256 intelligence
+        )
+    {
+        Character storage character = tokenIdToCharacter[tokenId];
+        return (
+            character.name,
+            character.class,
+            character.level,
+            character.agility,
+            character.strength,
+            character.intelligence
+        );
     }
 
     function getTokenURI(uint256 tokenId) public view returns (string memory) {
@@ -70,11 +136,12 @@ contract ChainBattles is ERC721URIStorage {
             );
     }
 
-    function mint() public {
+    function mint(string memory name) public {
         _tokenIds.increment();
         uint256 newItemId = _tokenIds.current();
         _safeMint(msg.sender, newItemId);
-        tokenIdToLevels[newItemId] = 0;
+        Character memory newCharacter = Character(name, "Warrior", 1, randMod(10), randMod(10), randMod(10));
+        tokenIdToCharacter[newItemId] = newCharacter;
         _setTokenURI(newItemId, getTokenURI(newItemId));
     }
 
@@ -84,8 +151,13 @@ contract ChainBattles is ERC721URIStorage {
             ownerOf(tokenId) == msg.sender,
             "You must own this NFT to train it!"
         );
-        uint256 currentLevel = tokenIdToLevels[tokenId];
-        tokenIdToLevels[tokenId] = currentLevel + 1;
+        // TODO: level up character and buff attributes
+        Character storage character = tokenIdToCharacter[tokenId];
+        character.level++;
+        character.agility = character.agility.add(randMod(5));
+        character.strength = character.strength.add(randMod(5));
+        character.intelligence = character.intelligence.add(randMod(5));
+        // tokenIdToLevels[tokenId] = currentLevel + 1;
         _setTokenURI(tokenId, getTokenURI(tokenId));
     }
 }
--- week-3/chainbattles/scripts/mint-token.js
+++ week-3/chainbattles/scripts/mint-token.js
@@ -8,14 +8,20 @@ async function main() {
     await chainBattles.deployed();
     console.log("Contract deployed to:", chainBattles.address);
 
-    await chainBattles.connect(owner).mint();
+    await chainBattles.connect(owner).mint("Paripi Koumei");
     const tokenId = 1;
     const uri = await chainBattles.connect(owner).getTokenURI(tokenId);
     console.log("=== Token URI ===")
     console.log(uri);
     const character = await chainBattles.connect(owner).generateCharacter(tokenId);
-    console.log("=== Character SVG ===")
+    console.log("=== Character SVG: Before train ===")
     console.log(character)
+
+    await chainBattles.connect(owner).train(tokenId);
+    const trainedCharacter = await chainBattles.connect(owner).generateCharacter(tokenId);
+    console.log("=== Character SVG: After train ===")
+    console.log(trainedCharacter)
+
 }
 
 main()
